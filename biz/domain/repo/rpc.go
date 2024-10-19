3361d6f4b70121d4467b93df3231dd833b563e7f
haoZhangran <haoranz527@gmail.com>
2024-10-19 23:26:45 +0800

    feat:测试跑通

--- biz/domain/repo/rpc.go
+++ biz/domain/repo/rpc.go
@@ -1,7 +1,11 @@
 package repo
 
-import "context"
+import (
+	"context"
+
+	"github.com/haoZhangran/common_sdk/sql/zproject/z_entity"
+)
 
 type IUserServiceRpcClient interface {
-	GetUserInfoByID(ctx context.Context, id string) // TODO(@yua) 需要先建表、确定User结构体
+	GetUserInfoByID(ctx context.Context, id string) (*z_entity.User, error) // TODO(@yua) 需要先建表、确定User结构体
 }
--- biz/infra/rpc/user_service.go
+++ biz/infra/rpc/user_service.go
@@ -1,1 +1,46 @@
 package rpc
+
+import (
+	"context"
+	"fmt"
+	"user_api/common/consts"
+
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/haoZhangran/common_sdk/sql/zproject/z_entity"
+
+	"github.com/haoZhangran/common_sdk/protoc_gen_code/zproject/user_service"
+	"google.golang.org/grpc"
+)
+
+type UserServiceRPCImpl struct {
+	client user_service.UserServiceClient
+}
+
+func InitUserServiceRPCImpl() *UserServiceRPCImpl {
+	conn, err := grpc.NewClient(consts.Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+		return nil
+	}
+	c := user_service.NewUserServiceClient(conn)
+	return &UserServiceRPCImpl{
+		client: c,
+	}
+}
+
+func (u UserServiceRPCImpl) GetUserInfoByID(ctx context.Context, id string) (*z_entity.User, error) {
+	req := &user_service.GetUserInfoByIDRequest{
+		ID: id,
+	}
+	resp, err := u.client.GetUserInfoByID(ctx, req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, nil
+	}
+
+	userInfo := &z_entity.User{
+		Name: resp.GetName(),
+	}
+	return userInfo, nil
+}
